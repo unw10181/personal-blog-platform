a0af60019723cd50ef463afa7c3a388fbe5e8e0c
unw10181 <uchennanwilliams@gmail.com>
2025-11-14 15:05:39 -0500

    js layout

---
+++ scipt.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('form')
+const title = document.getElementById('title')
+const titleError = document.getElementById('title-error')
+
+const content = document.getElementById('content')
+const contentError = document.getElementById('content-error')
